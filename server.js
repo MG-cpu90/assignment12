1084abbae8a2e99228792d98a12dc670df591275
Massimo Guida <massimo.guidamusic@gmail.com>
2020-09-02 10:45:27 -0400

    replace var connection with const connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 // init function = init();
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
